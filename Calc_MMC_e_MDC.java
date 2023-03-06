cc6a3927f0b654008290ee76012328a9a16b917d
DevSistemasCod <124713140+DevSistemasCod@users.noreply.github.com>
2023-03-05 22:16:31 -0300

    Update Calc_MMC_e_MDC.java

--- Calc_MMC_e_MDC.java
+++ Calc_MMC_e_MDC.java
@@ -4,32 +4,32 @@ import java.util.Scanner;
 
 public class Calc_MMC_e_MDC {
 	
-	public static int calcularMMC(int num1, int num2) {
-        int mmc = 1, divisor = 2, resto1, resto2;
+     public static int calcularMMC(int num1, int num2) {
+           int mmc = 1, divisor = 2, resto1, resto2;
         
-        while (num1 != 1 || num2 != 1) {
-        	resto1 = (num1 % divisor);
-        	resto2 = (num2 % divisor);
+           while (num1 != 1 || num2 != 1) {
+           	resto1 = (num1 % divisor);
+           	resto2 = (num2 % divisor);
         	
-        	if((resto1 == 0) || (resto2 == 0)) {
-                mmc = mmc * divisor;
+           	if((resto1 == 0) || (resto2 == 0)) {
+                   mmc = mmc * divisor;
                 
-                if (resto1 == 0) {
+                   if (resto1 == 0) {
                 	num1 = (num1/divisor);
-                }
-                if (resto2 == 0) {
+                   }
+                   if (resto2 == 0) {
                 	num2 = (num2/divisor);
+                   }
                 }
-            }
         	else {
-                divisor++;
-            }
-        }
+                  divisor++;
+                }
+           }
         
         return mmc;
      }
     
-    public static int calcularMDC(int num1, int num2) {
+     public static int calcularMDC(int num1, int num2) {
         int resto;
         
         resto = (num1 % num2);
@@ -40,12 +40,12 @@ public class Calc_MMC_e_MDC {
            resto = num1 % num2;
         }
         return num2;
-    }
+     }
 
 	
-	public static void main(String[] args) {
-		int opcao = 0, num1, num2, num3, num4, mmc, mdc;
-		Scanner lerTeclado = new Scanner(System.in);
+     public static void main(String[] args) {
+	int opcao = 0, num1, num2, num3, num4, mmc, mdc;
+	Scanner lerTeclado = new Scanner(System.in);
         
         
         while (opcao != 3) {

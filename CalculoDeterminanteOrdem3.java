4354d0410289b93de0f4def54164f3e1a8eab647
DevSistemasCod <124713140+DevSistemasCod@users.noreply.github.com>
2023-03-05 22:30:58 -0300

    Update CalculoDeterminanteOrdem3.java

--- CalculoDeterminanteOrdem3.java
+++ CalculoDeterminanteOrdem3.java
@@ -8,21 +8,20 @@ public class CalculoDeterminanteOrdem3 {
         double determinante = 0;
         
         for (int i = 0; i < 3; i++) {
-            determinante = determinante +(matriz[0][i] * matriz[1][(i+1)%3] * matriz[2][(i+2)%3]);
-            determinante = determinante -(matriz[0][i] * matriz[1][(i+2)%3] * matriz[2][(i+1)%3]);
+            determinante = determinante + (matriz[0][i] * matriz[1][(i+1)%3] * matriz[2][(i+2)%3]);
+            determinante = determinante - (matriz[0][i] * matriz[1][(i+2)%3] * matriz[2][(i+1)%3]);
         }
         return determinante;
     }
 
 	
-	public static void main(String[] args) {
-		int contador = 0;
-		double determinante;
-		double[] numeros;
-		double[][] matriz;
-		Scanner lerDados = new Scanner(System.in);
+    public static void main(String[] args) {
+	int contador = 0;
+	double determinante;
+	double[] numeros;
+	double[][] matriz;
+	Scanner lerDados = new Scanner(System.in);
 		
-
         // Lê os números da sequência
         System.out.println("Digite a sequência de números para preencher a matriz:");
         numeros = new double[9];
